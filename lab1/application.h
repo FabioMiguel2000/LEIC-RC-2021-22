fb6ecdc103a8e0f57202bb38096179f8020689be
Angela Cruz <up201806781@g.uporto.pt>
2021-12-22 11:48:32 +0000

    application comments

--- lab1/application.h
+++ lab1/application.h
@@ -25,17 +25,48 @@ struct dataFile {
     int fd;
 };
 
-
+/**
+ * @brief Parses app arguments
+ * 
+ * @param argc      Argument count
+ * @param arg      Argument vector
+ * @return int      number of port that has been passed by user, -1 if error
+ */
 int parseArgs(int argc, char** arg);
 
+/**
+ * @brief Expect UA to arrive from port.
+ * 
+ * @param fd   Port to expect the frame comes from
+ * @return int      On success, 0;-1 otherwise
+ */
 int receiver_UA(int fd);
-
+/**
+ * @brief Send SET message (a S-frame) to serial port.
+ * 
+ * @param fd   Port to send SET message to
+ * @return int      O on success, -1 otherwise
+ */
 int transmitter_SET(int fd);
-
+/**
+ * @brief Sends data packet.
+ * 
+ * @param fd          Piece of data from the file to be sent
+ * @return int        0 on success, -1 on error
+ */
 int sendPacket(int fd);
-
+/**
+ * @brief Initializes the application.
+ * 
+ * @param argc          Arguments count
+ * @param arg           Argument vector
+ * @return int          0 on success, -1 on error
+ */
 int main(int argc, char** arg);
-
+/**
+ * @brief Gets app arguments
+ * @return int     return 0 if success
+ */
 int getArgs();
 
 #endif
--- lab1/utils.h
+++ lab1/utils.h
@@ -24,11 +24,21 @@ struct timeElapsed {
 };
 
 int timeout,timeoutCount;
-
+/**
+ * @brief Handle the Alarm; Checks whether timeout has been reached
+ * @return void
+ */
 void timeoutHandler();
 
 void disconnectTimeout();
-
+/**
+ * @brief Log LL activity.
+ * 
+ * Prints error message in the right format
+ * 
+ * @param msg    array to put the message 
+ * @return void
+ */
 void logError(char *msg);
 
 void logSuccess(char *msg);
@@ -46,7 +56,12 @@ void endTimeElapsed();
 void logStats();
 
 void generateErrorBCC2(unsigned char *frame, int size, int stuffedBCC2Size);
-
+/**
+ * @brief Generates bcc1 errors to test application 
+ * 
+ * @param frame      frame used 
+ * @return int          Same as printf, or 0 if too verbose
+ */
 void generateErrorBCC1(unsigned char *frame);
 
 #endif
