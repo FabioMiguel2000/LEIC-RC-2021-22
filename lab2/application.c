b078eee259a2b961adcca898ebf2f8b064f2b32c
Fab <fabiomiguelchenhuang@gmail.com>
2022-01-09 12:21:22 +0000

    login added

--- lab2/application.c
+++ lab2/application.c
@@ -19,6 +19,8 @@ int parseArgs(int argc, char **argv)
 
         application_params.host = strtok(removedHeader, "/");
         application_params.url_path = strtok(NULL, ";");
+        application_params.user = "anonymous";
+        application_params.pass = "anonymous";
     }
     else
     {
@@ -35,43 +37,46 @@ int parseArgs(int argc, char **argv)
         application_params.user = strtok(user_pass, ":");
         application_params.pass = strtok(NULL, ":");
     }
-    if(application_params.host == NULL || application_params.url_path == NULL){
+    if (application_params.host == NULL || application_params.url_path == NULL)
+    {
         return -1;
     }
     char msg[MAX_SIZE];
-    sprintf(msg, "\n\tuser = %s\n\tpass = %s\n\thost = %s\n\turl path = %s\n", 
-    application_params.user == NULL?"anonymous": application_params.user,
-    application_params.pass == NULL? "anonymous": application_params.pass,
-    application_params.host,
-    application_params.url_path);
+    sprintf(msg, "\n\tuser = %s\n\tpass = %s\n\thost = %s\n\turl path = %s\n",
+            application_params.user,
+            application_params.pass,
+            application_params.host,
+            application_params.url_path);
     logInfo(msg);
     return 0;
 }
 
-struct hostent* getip(char *host) {
+struct hostent *getip(char *host)
+{
     struct hostent *h;
 
-/**
- * The struct hostent (host entry) with its terms documented
+    /**
+     * The struct hostent (host entry) with its terms documented
 
-    struct hostent {
-        char *h_name;    // Official name of the host.
-        char **h_aliases;    // A NULL-terminated array of alternate names for the host.
-        int h_addrtype;    // The type of address being returned; usually AF_INET.
-        int h_length;    // The length of the address in bytes.
-        char **h_addr_list;    // A zero-terminated array of network addresses for the host.
-        // Host addresses are in Network Byte Order.
-    };
+        struct hostent {
+            char *h_name;    // Official name of the host.
+            char **h_aliases;    // A NULL-terminated array of alternate names for the host.
+            int h_addrtype;    // The type of address being returned; usually AF_INET.
+            int h_length;    // The length of the address in bytes.
+            char **h_addr_list;    // A zero-terminated array of network addresses for the host.
+            // Host addresses are in Network Byte Order.
+        };
 
-    #define h_addr h_addr_list[0]	The first address in h_addr_list.
-*/
-    if ((h = gethostbyname(host)) == NULL) {
+        #define h_addr h_addr_list[0]	The first address in h_addr_list.
+    */
+    if ((h = gethostbyname(host)) == NULL)
+    {
         logError("gethostbyname()");
         exit(-1);
     }
 
     char msg[MAX_SIZE];
-    sprintf(msg, "\n\tHost name  : %s\n\tIP Address : %s\n", h->h_name,inet_ntoa(*((struct in_addr *) h->h_addr)));
+    sprintf(msg, "\n\tHost name  : %s\n\tIP Address : %s\n", h->h_name, inet_ntoa(*((struct in_addr *)h->h_addr)));
     logInfo(msg);
 
     return h;
@@ -85,7 +90,7 @@ void segmentation_fault_handler()
 
 int main(int argc, char **argv)
 {
-    
+
     if (parseArgs(argc, argv) < 0)
     {
         logUsage();
@@ -99,47 +104,98 @@ int main(int argc, char **argv)
     // size_t bytes;
 
     /*server address handling*/
-    bzero((char *) &server_addr, sizeof(server_addr));
+    bzero((char *)&server_addr, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr(inet_ntoa(*((struct in_addr *) h->h_addr)));    /*32 bit Internet address network byte ordered*/
-    server_addr.sin_port = htons(FTP_PORT);        /*server TCP port must be network byte ordered */
+    server_addr.sin_addr.s_addr = inet_addr(inet_ntoa(*((struct in_addr *)h->h_addr))); /*32 bit Internet address network byte ordered*/
+    server_addr.sin_port = htons(FTP_PORT);                                             /*server TCP port must be network byte ordered */
 
     /*open a TCP socket*/
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
         logError("socket()");
         exit(-1);
     }
     /*connect to the server*/
     if (connect(sockfd,
-                (struct sockaddr *) &server_addr,
-                sizeof(server_addr)) < 0) {
+                (struct sockaddr *)&server_addr,
+                sizeof(server_addr)) < 0)
+    {
         logError("connect()");
         exit(-1);
     }
 
-    char buff[2];
+    // Server response after connection
+    char buff[MAX_SIZE];
     int res = read(sockfd, buff, 1);
     int count = 0;
     printf(">>>Server Response:\n");
-    while(res != -1){
+    while (res != -1)
+    {
         printf("%c", buff[0]);
         res = read(sockfd, buff, 1);
-        if(buff[0] == '\n'){
-            count ++;
+        if (buff[0] == '\n')
+        {
+            count++;
         }
-        if(count == 7){
+        if (count == 10)
+        {
             break;
         }
     }
-    
+    memset(buff, 0, MAX_SIZE * sizeof(char));
+    // Send User
+    sprintf(buff, "user %s", application_params.user);
+    res = write(sockfd, buff, strlen(buff));
+    res = write(sockfd, "\n", 1);
+
+    printf("Client:\n%s\n", buff);
+
+    memset(buff, 0, MAX_SIZE * sizeof(char));
+
+    // Server response after sending user
+    res = read(sockfd, buff, 1);
+    printf(">>>Server Response:\n");
+    count = 0;
+    while (res != -1)
+    {
+        printf("%c", buff[0]);
+        res = read(sockfd, buff, 1);
+        if (buff[0] == '\n')
+        {
+            printf("%c", buff[0]);
+            break;
+        }
+    }
+    memset(buff, 0, MAX_SIZE * sizeof(char));
+
+    // Send Pass
+    sprintf(buff, "pass %s", application_params.pass);
+    res = write(sockfd, buff, strlen(buff));
+    res = write(sockfd, "\n", 1);
+
+    memset(buff, 0, MAX_SIZE * sizeof(char));
+
+    // Server response after sending user
+    res = read(sockfd, buff, 1);
+    printf(">>>Server Response:\n");
+    count = 0;
+    while (res != -1)
+    {
+        printf("%c", buff[0]);
+        res = read(sockfd, buff, 1);
+        if (buff[0] == '\n')
+        {
+            printf("%c", buff[0]);
+            break;
+        }
+    }
+
     // if(res == -1){
     //     logError("No response from server\n");
     //     exit(-1);
     // }
     // logServer(buff);
-	
 
-	
     // /*send a string to the server*/
     // bytes = write(sockfd, buf, strlen(buf));
     // if (bytes > 0)
